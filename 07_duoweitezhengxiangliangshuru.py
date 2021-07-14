9c6417db8a05a9d8c1f7a3e0e629690f1c1cbb13
BingzheJiang <1219255570@qq.com>
2021-07-14 17:08:29 +0800

    添加激活函数_逻辑斯蒂回归

---
+++ 07_duoweitezhengxiangliangshuru.py
@@ -0,0 +1,12 @@
+#https://blog.csdn.net/bit452/article/details/109682078
+#1.乘的权重(w)都一样，加的偏置(b)也一样。b变成矩阵时使用广播机制。神经网络的参数w和b是网络需要学习的，其他是已知的。
+#2.学习能力越强，有可能会把输入样本中噪声的规律也学到。我们要学习数据本身真实数据的规律，学习能力要有泛化能力
+#3.该神经网络共3层；第一层是8维到6维的非线性空间变换，第二层是6维到4维的非线性空间变换，第三层是4维到1维的非线性空间变换。
+#4.本算法中torch.nn.Sigmoid() # 将其看作是网络的一层，而不是简单的函数使用
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+#prepare dataset
+xy=np.loadtxt('diabetes.csv',delimiter=',',dtype=np.float32)
+
