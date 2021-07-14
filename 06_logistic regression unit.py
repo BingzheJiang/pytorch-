709a59af1b3e6759743270b214ee44ff01526ddf
BingzheJiang <1219255570@qq.com>
2021-07-14 16:23:00 +0800

    添加激活函数_逻辑斯蒂回归

---
+++ 06_logistic regression unit.py
@@ -0,0 +1,44 @@
+#逻辑斯蒂回归和线性模型的明显区别是在线性模型的后面，添加了激活函数(非线性变换)
+#分布的差异：KL散度，cross-entropy交叉熵
+#BCELoss 是CrossEntropyLoss的一个特例，只用于二分类问题，而CrossEntropyLoss可以用于二分类，也可以用于多分类。
+#预测与标签越接近，BCE损失越小。
+import torch
+
+#prepare dataset
+#x,y是矩阵，3行1列，也就是说总共有3个数据，每个数据只有1个特征
+x_data = torch.Tensor([[1.0], [2.0], [3.0]])
+y_data = torch.Tensor([[0], [0], [1]])
+
+#design model using class
+class LogisticRegressionModel(torch.nn.Module):
+    def __init__(self):
+        super(LogisticRegressionModel,self).__init__()
+        self.linear=torch.nn.Linear(1,1)
+
+    def forward(self,x):
+        y_pred=torch.sigmoid(self.linear(x))
+        return y_pred
+model=LogisticRegressionModel()
+
+
+# construct loss and optimizer
+# 默认情况下，loss会基于element平均，如果size_average=False的话，loss会被累加。
+criterion=torch.nn.BCELoss(size_average=False)
+optimizer=torch.optim.SGD(model.parameters(),lr=0.01)
+
+#training cycle forward,backward,update
+for epoch in range(1000):
+    y_pred=model(x_data)
+    loss=criterion(y_pred,y_data)
+    print(epoch,loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print('w=',model.linear.weight.item())
+print('b=',model.linear.bias.item())
+
+x_test=torch.Tensor([[4.0]])
+y_test=model(x_test)
+print('y_pred=',y_test.data)
--- test.py
+++ test.py
@@ -1,11 +1,36 @@
 #w是Tensor(张量类型)，Tensor中包含data和grad，data和grad也是Tensorl。grad初始为None，调用l.backward()方法后w.grad为Tensor，故更新w.data时需使用w.grad.data。如果w需要计算梯度，那构建的计算图中，跟w相关的tensor都默认需要计算梯度
 import torch
-a=torch.tensor([1.0])
-a.requires_grad=True
-print(a)
-print(a.data)
-print(a.type())# a的类型是tensor
-print(a.data.type())# a.data的类型是tensor
-print(a.grad)
-print(type(a.grad))
+import math
+# a=torch.tensor([1.0])
+# a.requires_grad=True
+# print(a)
+# print(a.data)
+# print(a.type())# a的类型是tensor
+# print(a.data.type())# a.data的类型是tensor
+# print(a.grad)
+# print(type(a.grad))
 
+pred = torch.tensor([[-0.2], [0.2], [0.8]])
+target = torch.tensor([[0.0], [0.0], [1.0]])
+
+sigmoid = torch.nn.Sigmoid()
+pred_s = sigmoid(pred)
+print(pred_s)
+"""
+pred_s 输出tensor([[0.4502],[0.5498],[0.6900]])
+0*math.log(0.4502)+1*math.log(1-0.4502)
+0*math.log(0.5498)+1*math.log(1-0.5498)
+1*math.log(0.6900) + 0*log(1-0.6900)
+"""
+result = 0
+i = 0
+for label in target:
+    if label.item() == 0:
+        result += math.log(1 - pred_s[i].item())
+    else:
+        result += math.log(pred_s[i].item())
+    i += 1
+result /= 3
+print("bce：", -result)
+loss = torch.nn.BCELoss()
+print('BCELoss:', loss(pred_s, target).item())
